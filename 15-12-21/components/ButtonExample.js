eb14c64be073bcf8892165742683c3cf3822307a
MintedKitten <87274176+MintedKitten@users.noreply.github.com>
2021-12-15 11:56:14 +0700

    Send in Work

---
+++ 15-12-21/components/ButtonExample.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {View, Text, StyleSheet, SafeAreaView, Button} from 'react-native';
+
+const ButtonExample = () => {
+  const onPressAlert = () => {
+    alert('Woah');
+  };
+
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <View style={styles.container}>
+        <Text>Button Example</Text>
+        <Button
+          title="Click Me"
+          onPress={() => {
+            onPressAlert();
+          }}
+          color="#841584"
+        />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default ButtonExample;
