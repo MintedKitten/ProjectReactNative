a748e5f2c06e1c7dc0741f60cb3c6ce8bee1f0ef
MintedKitten <87274176+MintedKitten@users.noreply.github.com>
2021-12-15 12:28:53 +0700

    test

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import ModalExample from './components/ModalExample';
+// import AlertExample from './components/AlertExample';
+
+const App = () => {
+  return (
+    // <AlertExample />
+    <ModalExample />
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
