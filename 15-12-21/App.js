372d046f4152978845680a4f504c7022fea0ab7a
MintedKitten <87274176+MintedKitten@users.noreply.github.com>
2022-01-05 11:18:18 +0700

    Turn in Work

--- 15-12-21/App.js
+++ 15-12-21/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
-import ModalExample from './components/ModalExample';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import ModalExample from "./components/ModalExample";
 // import AlertExample from './components/AlertExample';
 
 const App = () => {
